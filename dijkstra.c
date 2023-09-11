d1185074fa0250485ed23f6817fee27d54c87b24
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-09-10 19:16:47 -0500

    Creada matriz del grafo

--- dijkstra.c
+++ dijkstra.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_NODES 20
+
 typedef struct pq_struct PQ;
 
 typedef struct node node;
@@ -28,6 +30,49 @@ PQ *pq_create(int capacity) {
   pq->posArray = (int *)malloc(capacity * sizeof(int));
   pq->pqArray = (node *)malloc(capacity * sizeof(node));
   return pq;
+}
+
+int number_of_nodes = 0;
+int graph[MAX_NODES][MAX_NODES];
+char *nodes[MAX_NODES];
+int is_directed = 0;
+
+char *trimwhitespace(char *str) {
+  char *end;
+  // Trim leading space
+  while (isspace((unsigned char)*str))
+    str++;
+  if (*str == 0) // All spaces?
+    return str;
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while (end > str && isspace((unsigned char)*end))
+    end--;
+  // Write new null terminator character
+  end[1] = '\0';
+  return str;
+}
+
+// Returns -1 if node is not present
+int get_index_of_node(char *node_to_check) {
+  int value = -1;
+  for (int i = 0; i < number_of_nodes; i++) {
+    if (0 == (strcmp(nodes[i], node_to_check))) {
+      value = i;
+      break;
+    }
+  }
+  // printf("Index: %d\n", value);
+  return value;
+}
+
+void print_graph() {
+  for (int i = 0; i < number_of_nodes; i++) {
+    for (int j = 0; j < number_of_nodes; j++) {
+      printf("%d ", graph[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 // argc es el número de argumentos que se le pasan al programa incluyendo el nombre del programa
@@ -70,8 +115,8 @@ int main(int argc, char *argv[]) {
           *str = '\0';
         }
       }
-      int esDirigido = strcmp(line, "true");
-      if (esDirigido == 0) {
+      if (strcmp(line, "true") == 0) {
+        is_directed = 1;
         printf("Es dirigido\n");
       } else {
         printf("No es dirigido\n");
@@ -79,14 +124,59 @@ int main(int argc, char *argv[]) {
       ++line_number;
       continue;
     }
-    char *token; // Puntero al token
-    // char *s = NULL; // Puntero al final de la línea
-    int i = 0; // Número de token
-
+    char *token = strtok_r(line, ";", &line); // Puntero al token
     // Leemos los tokens de la línea
-    while ((token = strtok_r(line, ";", &line))) {
+    while (token != NULL) {
+      int i = 0; // auxiliar
+      int index = -1;
+      int destination = -1;
+      int weight = -1;
+      char *end_token;
+      char *token_inner = strtok_r(token, ",", &end_token);
+      while (token_inner != NULL && i < 3) {
+        if (i == 0) {
+          token_inner = trimwhitespace(token_inner);
+          token_inner += 1;
+          index = get_index_of_node(token_inner);
+          if (index == -1) {
+            index = number_of_nodes;
+            nodes[index] = malloc(sizeof(char) * (strlen(token_inner) + 1));
+            strcpy(nodes[index], token_inner);
+            ++number_of_nodes;
+          }
+        } else if (i == 1) {
+          token_inner = trimwhitespace(token_inner);
+          destination = get_index_of_node(token_inner);
+          if (destination == -1) {
+            destination = number_of_nodes;
+            nodes[destination] = malloc(sizeof(char) * (strlen(token_inner) + 1));
+            strcpy(nodes[destination], token_inner);
+            ++number_of_nodes;
+          }
+        } else if (i == 2) {
+          token_inner = trimwhitespace(token_inner);
+          char *s = token_inner;
+          // This while is to erase the the character ) and replace it by \n in order of no affect the atoi cast!
+          while (*s != ')') {
+            ++s;
+          }
+          *s = '\0';
+          weight = atoi(token_inner);
+        } else {
+          printf("Parsing error. Not possible four values for edege\n");
+        }
+        i++;
+        token_inner = strtok_r(NULL, ",", &end_token);
+      }
+      graph[index][destination] = weight;
+      if (is_directed == 0) {
+        graph[destination][index] = weight;
+      }
+      token = strtok_r(NULL, ";", &line);
     }
   }
+
+  print_graph();
 
   // Cerramos el fichero
   fclose(fp);
